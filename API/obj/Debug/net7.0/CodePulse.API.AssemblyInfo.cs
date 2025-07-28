6ed65b467ae401e434ebd45d7f43476ece18e25d
mohitsonu <mohitsonu33@gmail.com>
2025-07-29 03:02:27 +0530

    1

--- API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+312a9a98cf3370c29a9018cb7f0437baae264a94")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
