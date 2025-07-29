b1fdffaea5fe4446cceb76c08e122f06e79aa019
mohitsonu <mohitsonu33@gmail.com>
2025-07-29 22:46:24 +0530

    8

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://my-blog-b2pu.onrender.com'
+  // apiBaseUrl: 'https://my-blog-b2pu.onrender.com'
+  apiBaseUrl: 'https://blog-b8d99.web.app/',
 };
